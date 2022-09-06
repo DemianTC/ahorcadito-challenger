3e61e72fb8d612a087372ed2810857681f9ee02d
Damian Tec <piedrangular@live.com.mx>
2022-09-06 12:14:13 -0500

    se actualizaron imagenes y teclado

--- juego.js
+++ juego.js
@@ -2,14 +2,17 @@ let palabrita;
 let cant_errores = 0;  //cuantas veces me equivoque
 let cant_aciertos = 0; //cuantas letras acerte 
 
+
+
+
 const palabras = [
-    "ardilla",  /*0*/
-    "botitas",   /*1*/
-    "camion",   /*2*/
-    "coyote",   /*3*/
-    "cinema",   /*4*/
-    "tortilla",  /*5*/
-    "trabajar",    /*6*/
+    "ardilla", "java",  /*0*/
+    "botitas",   "challenger", /*1*/
+    "camion",   "motocicleta", /*2*/
+    "coyote",   "papalote", /*3*/
+    "cinema",   "tigre",  /*4*/
+    "tortilla",  "brasil", /*5*/
+    "trabajar",    "Cristian", /*6*/
     "ballena"   /*7*/
 ];
 
@@ -22,6 +25,10 @@ const getValueInput = () =>{
     document.getElementById("valueInput").innerHTML = inputValue; 
   }
 
+
+
+
+
 function id( str ){
     return document.getElementById( str );
 }
@@ -30,6 +37,7 @@ function obtener_random( num_min, num_max ){
     const amplitud_valores = num_max - num_min; //valor más alto - valor más bajo del random... (7 - 0)
     const valor_al_azar = Math.floor( Math.random( ) * amplitud_valores ) + num_min; /* 5 - 15 = 10 + 5 */
     return valor_al_azar;
+    
 }
 
 /* click en iniciar juego */
